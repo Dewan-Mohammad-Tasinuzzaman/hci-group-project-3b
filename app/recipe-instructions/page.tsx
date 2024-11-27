875ecb7ed6a8262aacdd58f7989e9880d11ad1b2
Dewan Mohammad Tasinuzzaman <dewanmohammad.tasinuzzaman@ontariotechu.net>
2024-11-27 07:44:47 -0500

    Project Setup

---
+++ app/recipe-instructions/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const RecipeInstructionsPage = () => {
+  return (
+    <div>RecipeInstructionsPage</div>
+  )
+}
+
+export default RecipeInstructionsPage
---
+++ app/recipes/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const RecipesPage = () => {
+  return (
+    <div>RecipesPage</div>
+  )
+}
+
+export default RecipesPage
